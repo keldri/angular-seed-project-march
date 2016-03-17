4bdc800ddfbced85aab3ffe5d910b0127965ec09
Kate Eldridge <kathryneeldridge@gmail.com>
2016-03-16 22:21:32 -0400

    changed app name to templates in app.js

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('templateStore', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'templateStore.view1',
+  'templateStore.view2',
+  'templateStore.templates'
 ]).
+//if utl is not = to view1, view2 or temps, load view 1
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/templates'});
 }]);
+ 
